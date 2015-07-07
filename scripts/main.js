2979570d11dc7ff706c3d52c1507a43bc55c62c6
Kristen Rieger <karieger1@gmail.com>
2015-07-06 23:59:38 -0500

    figuring out components?

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,73 @@
 var React = require('react');
+var Backbone = require('backbone');
+Backbone.$ = require('jquery');
 
 console.log('application running');
 
-// playing with bootstrap themes:
+//set up router:
 
+var App = Backbone.Router.extend({
+	routes: {
+		'': 'home',
+		'home': 'home',
+		'findThingsMap':   'findThingsMap',
+		'itemDetail': 	   'itemDetail',
+		'findThingsList':  'findThingsList',
+		'giverInfo':       'giverInfo',
+		'listThings':      'listThings',
+		'listSuccess':     'listSuccess',
+		'aboutUs': 	       'aboutUs'
+	},
+	home: function() {
+		// React.render(
+		// 	<HomePage categories={cat} />,
+		// 	containerEl
+		// );
+	},
+	findthingsMap: function() {
+		// React.render(
+		// 	<CategoryPage />,
+		// 	containerEl
+		// );
+	},
+	findThingsList: function() {
+		// React.render(
+		// 	<CategoryPage />,
+		// 	containerEl
+		// );
+	},
+	itemDetail: function() {
+		// React.render(
+		// 	<CategoryPage />,
+		// 	containerEl
+		// );
+	},
+	giverInfo: function() {
+		// React.render(
+		// 	<CategoryPage />,
+		// 	containerEl
+		// );
+	},
+	listThings: function() {
+		// React.render(
+		// 	<CategoryPage />,
+		// 	containerEl
+		// );
+	},
+	listSuccess: function() {
+		// React.render(
+		// 	<SearchPage />,
+		// 	containerEl
+		// );
+	},
+	aboutUs: function() {
+		// React.render(
+		// 	<ProductPage />,
+		// 	containerEl
+		// );
+	}
+});
+
+var myApp = new App();
+
+Backbone.history.start();
---
+++ scripts/models/listingModel.js
@@ -0,0 +1,21 @@
+var Backbone = require("backbone");
+var $ = require("jquery");
+Backbone.$ = $;
+
+module.exports = Backbone.Model.extend({
+	defaults: {
+		id: null,
+		title: null,
+		description: null,
+		itemCondition:
+		category: 
+		location:
+		userID:
+		userName:
+		userEmail:
+		userPhone:
+		userAddress:
+		userZip:
+		itemImage:
+	}
+});
